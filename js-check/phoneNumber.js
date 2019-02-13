5f9a5ce7d79b0bd6f948c33b79fb48899d677b94
Kathy Cuevas <meetnerdykat@gmail.com>
2019-02-13 17:13:14 -0500

    Initial commit on GitHub repo. HTML and CSS and Javascript code challenges.

---
+++ js-check/phoneNumber.js
@@ -0,0 +1,20 @@
+//
+// Phone Number
+// Finish the rest of 'phoneNumber()' function to satisfy the test
+// file located @ /test/main_test.spec.js
+//
+// To pass, your function should:
+// remove parenthesis, spaces, and hyphens as in "(321) 321-4321"
+// remove dots as in "321.321.4321"
+
+const phoneNumber = pNum => {
+  var cleanNum = pNum.replace(/[^\w]/g, '');
+  return cleanNum;
+};
+phoneNumber('(813) .45#$@!&*()-+!@6-789.8');
+
+module.exports = phoneNumber;
+// Note: It's not neccessary to have all code into the 'phoneNumber'
+// function. Feel free to make as many functions as you see
+// fit. Just be sure to leave phoneNumber as an exported
+// method as you found it
